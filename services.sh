ec2829e94f68f043fd06041edce4d7c839ac6d5d
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-03-08 16:15:35 +0530

    Update managed host dockerfile

--- services.sh
+++ services.sh
@@ -1,2 +1,4 @@
 sudo service ssh start
+sudo service docker start
+sudo service filebeat start
 sleep infinity
--- temp.sh
+++ temp.sh
@@ -1,5 +1,5 @@
 # script to run managed hosts for ansible
 docker build -f Dockerfile.managed -t managed-image .
 
-docker run -dt -P --name managed1 managed-image
-docker run -dt -P --name managed2 managed-image
+docker run --privileged --cap-add=NET_ADMIN -dt -P --name managed1 managed-image
+docker run --privileged --cap-add=NET_ADMIN -dt -P --name managed2 managed-image

594c46aee7872c1cc00e274fb9d5a512ddb16482
Kaustubh Nair <kaustubh.nair108@gmail.com>
2021-03-10 11:09:50 +0530

    Fix dockerfile for managed hosts

--- temp.sh
+++ temp.sh
@@ -1,4 +1,5 @@
 # script to run managed hosts for ansible
+docker rm -f $(docker ps -a | grep managed | awk '{print $1}' )
 docker build -f Dockerfile.managed -t managed-image .
 
 docker run --privileged --cap-add=NET_ADMIN -dt -P --name managed1 managed-image
